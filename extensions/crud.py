a440cc8efedca12fb2845bde75d315f84de7b746
matias endres <>
2021-04-25 04:50:33 +0000

    crud working

--- extensions/crud.py
+++ extensions/crud.py
@@ -1,11 +1,52 @@
 from discord.ext import commands
 import discord
+from replit import db
 
 class Crud(commands.Cog):
 
-    @commands.comand()
-    async def deb(ctx):
-        await ctx.send('its working')
+    @commands.command()
+    async def key_list(self, ctx):
+        """| shows all the keys"""
+        key_list = f'key_list = {db.keys()}'
+        await ctx.send(key_list)
+
+    @commands.command()
+    async def get(self, ctx, key):
+        """| shows all values of a key"""
+        await ctx.send(f'{key} = {list(db[key])}')
+
+    @commands.command()
+    async def delete(self, ctx, key, *args):
+        """| delete a key or multiple values from key"""
+        if args:
+            values = db[key]
+            for value in args:
+                values.remove(value)
+            db[key] = values
+        else:
+            del db[key]
+
+        await ctx.send(f'perfecto, eliminado!')
+
+    @commands.command()
+    async def add(self, ctx, key, *args):
+        """| add key or multiple new values"""
+        if key in db.keys():
+            values = db[key]
+            for value in args:
+                values.append(value)
+            db[key] = values
+        else:
+            if args:
+                values = []
+                for value in args:
+                    values.append(value)
+                db[key] = values  
+            else:
+                db[key] = []
+
+        await ctx.send(f'perfecto, añadido!')
 
 def setup(bot):
     bot.add_cog(Crud(bot))
+
--- main.py
+++ main.py
@@ -30,76 +30,30 @@ async def on_ready():
 	    f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n'
 	)
 
-
-def add_phrase(command):
-	key = command.split(':', 1)[0]
-	value = command.split(':', 1)[1]
-	if key in db.keys():
-		values = db[key]
-		values.append(value)
-		db[key] = values
-	else:
-		db[key] = [value]
-
-	return f'perfecto, {key} : {value} añadido!'
-
-
-def delete_phrase(command):
-
-	if ':' in command:
-		key = command.split(':', 1)[0]
-		value = command.split(':', 1)[1]
-		phrases = db[key]
-		phrases.remove(value)
-		db[key] = phrases
-	else:
-		del db[command]
-
-	return 'eliminado!'
-
-
 @bot.event
 async def on_message(message):
 
-	msg = message.content.lower()
-	bot_name = bot.user.name
+    msg = message.content.lower()
+    bot_name = bot.user.name
 
-	if msg != bot.user and str(bot_name) in msg:
-		response = ''
+    if msg != bot.user and str(bot_name) in msg:
 
-		##GET KEY_LIST
-		if msg.startswith(f'${bot_name} key_list'):
-      for any(word in msg for word in db[key]) and 'trigger'
-        
-      
-        
-		##GET A KEY'S VALUES
-		elif msg.startswith(f'${bot_name} get '):
-			command = msg.split(f'${bot_name} get ', 1)[1]
-			response = f'{command} = {list(db[command])}'
-		##DELETE A KEY OR VALUE
-		elif msg.startswith(f'${bot_name} delete'):
-			command = msg.split(f'${bot_name} delete ', 1)[1]
-			response = delete_phrase(command)
-		##ADD A KEY OR A VALUE
-		elif msg.startswith(f'${bot_name} add '):
-			command = msg.split(f'${bot_name} add ', 1)[1]
-			response = add_phrase(command)
-		else:
-			##INTERACTIVE MODULE
-			##toma como trigger trigger_tema y da una respuesta de response_tema
-			##notar que tema en trigger_ y response_ debe ser igual
-			for key in db:
-				if any(word in msg for word in db[key]) and 'trigger_' in key:
-					new_key = key.replace('trigger_', 'response_')
-					response += random.choice(db[new_key])
+        response = ''
+        ##INTERACTIVE MODULE
+        ##toma como trigger trigger_tema y da una respuesta de response_tema
+        ##notar que tema en trigger_ y response_ debe ser igual
+        for key in db:
+            if any(word in msg for word in db[key]) and 'trigger_' in key:
+                new_key = key.replace('trigger_', 'response_')
+                response += random.choice(db[new_key])
 
-			if response == '':
-				response = 'dime'
+        if response == '':
+            response = 'dime'
 
-		await message.channel.send(response)
+        await message.channel.send(response)
 
-	await bot.process_commands(message)
+
+    await bot.process_commands(message)
 
 
 keep_alive()
